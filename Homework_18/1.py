8f42fbce29c1e888db8b706328db2e853d244974
Tatsiana <timoha-tatiana@mail.ru>
2023-03-20 20:27:40 +0300

    Homework 18

--- Homework_18/1.py
+++ Homework_18/1.py
@@ -1,13 +1,14 @@
 import requests
 import telebot
-from config import TOKEN, BANK_URL
+from config import TOKEN, BANK_URL, DOG_URL
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 url = BANK_URL
+url_dog = DOG_URL
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hi! I'm a crypto bot to output the top-5 crypto currencies, enter the command /crypto")
+    bot.reply_to(message, "Hi! I'm a crypto and dog bot. \nIf you want to get information about crypto currency, enter /crypto\nIf you want to get a picture with a dog, enter /dog")
 
 @bot.message_handler(commands=['crypto'])
 def get_crypto_info(message):
@@ -19,5 +20,12 @@ def get_crypto_info(message):
         price_change_percent = data['priceChangePercent']
         crypto_data += f'\n{symbol}: {last_price} ({price_change_percent}%)'
     bot.send_message(message.chat.id, crypto_data)
+
+@bot.message_handler(commands=['dog'])
+def get_dog(message):
+    response = requests.get(url_dog)
+    image_url = response.json()['message']
+    bot.send_photo(message.chat.id, image_url)
+
 
 bot.infinity_polling()
